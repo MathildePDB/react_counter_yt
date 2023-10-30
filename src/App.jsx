59ab98a8eed2042eb21a8813dfec1e9759f8a71c
MathildePDB <114467024+MathildePDB@users.noreply.github.com>
2023-10-30 11:27:34 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Button from "./components/Button";
+import Counter from "./components/Counter";
+import "./Styles.css"
+
+export default function App() {
+  const [number, setNumber] = useState(0)
+
+  function add() {
+    setNumber(number + 1)
+  }
+
+  function substract() {
+    setNumber(number - 1)
+  }
+
+  return (
+    <div className="App">
+      <h1>Compteur</h1>
+      <Counter>{number}</Counter>
+      <Button click={substract} content="-" />
+      <Button click={add} content="+" />
+    </div>
+  )
+}
